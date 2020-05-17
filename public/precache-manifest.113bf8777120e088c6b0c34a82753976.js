6a90beafc100acf2dac0314147e68c81505a779a
problemsolver03 <lnov36630@gmail.com>
2020-05-17 19:48:54 +0530

    relase

---
+++ public/precache-manifest.113bf8777120e088c6b0c34a82753976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5721e754d10a92f5b91607b09c7090b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc56a40b4551061cbdaf",
+    "url": "/static/css/main.4a789c9b.chunk.css"
+  },
+  {
+    "revision": "aa28253cfecd5462bd4b",
+    "url": "/static/js/2.0010b1e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0010b1e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc56a40b4551061cbdaf",
+    "url": "/static/js/main.5d8b69dc.chunk.js"
+  },
+  {
+    "revision": "94b12379f7df2aa464a4",
+    "url": "/static/js/runtime-main.49ac3ba5.js"
+  }
+]);
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 router.post('/fileprocessing', function (req, res, next) { 
  
